e03721a85e0cb49cce5f86f26549765573701b79
Nadilson <nadilson@protonmail.com>
2022-06-23 03:06:21 -0300

    Projeto criado

---
+++ src/main/java/com/nadilson/nadfirebasedb/NadfirebasedbApplication.java
@@ -0,0 +1,13 @@
+package com.nadilson.nadfirebasedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NadfirebasedbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NadfirebasedbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nadilson/nadfirebasedb/NadfirebasedbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadilson.nadfirebasedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NadfirebasedbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
